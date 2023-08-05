432d92cd2b82da6fd83322f89da3f443883a5d14
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-08-05 10:42:29 -0400

    Rename American Express test

--- simplecardbrand/test/americanexpress_test.exs
+++ simplecardbrand/test/americanexpress_test.exs
@@ -1,12 +1,12 @@
 defmodule SimpleCardBrandAmericanExpressTest do
   @moduledoc """
   American Express specific tasks.
-
   """
   use ExUnit.Case
 
-  test "AmericanExpress" do
+  test "AmericanExpressWikipedia" do
     assert SimpleCardBrand.card_brand("345678901234567") == {:ok, :americanexpress}
     assert SimpleCardBrand.card_brand("3457", 15) == {:ok, :americanexpress}
+    assert SimpleCardBrand.card_brand("37", 15) == {:ok, :americanexpress}
   end
 end
