a20cfa8241e2a80f844695d9ad65d2666b308e59
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-08-06 14:04:08 -0400

    Add parameterized testing
    Correct JCB tests.

--- test/jcb_test.exs
+++ test/jcb_test.exs
@@ -2,13 +2,26 @@ defmodule SimpleCardBrandJCB do
   @moduledoc """
   JCB tests.
   """
-  use ExUnit.Case
 
-  test "JCBWikipedia" do
-    for pan <- 3528..3589 do
-      for pan_length <- 16..19 do
-        assert SimpleCardBrand.card_brand(Integer.to_string(pan), pan_length) == {:ok, :jcb}
-      end
-    end
+  use ExUnit.Case
+  use ExUnit.Parameterized
+
+  # description
+  test_with_params "JCBWikipedia",
+                   # test case
+                   fn pan, pan_length, expected ->
+                     assert SimpleCardBrand.card_brand(pan, pan_length) == {:ok, expected}
+                   end do
+    [
+      # parameters
+      {"3528", 16, :jcb},
+      {"3530", 17, :jcb},
+      {"3540", 18, :jcb},
+      {"3550", 19, :jcb},
+      # {"3560", 16, :jcb},  # RuPay Co-brand
+      {"3570", 17, :jcb},
+      {"3580", 18, :jcb},
+      {"3589", 19, :jcb}
+    ]
   end
 end
